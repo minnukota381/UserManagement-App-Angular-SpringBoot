a84f31d5fbdc425e8d416cf902a59b9716c743bb
minnukota381 <minnukota381@gmail.com>
2024-09-08 10:53:28 +0530

    "Refactor user-create component to use Angular Material and reactive forms"

--- user-management/src/app/user-create/user-create.component.ts
+++ user-management/src/app/user-create/user-create.component.ts
@@ -1,22 +1,49 @@
-// src/app/user-create/user-create.component.ts
-import { Component } from '@angular/core';
+// user-create.component.ts
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
 import { User } from '../models/user.model';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-create',
   templateUrl: './user-create.component.html',
   styleUrls: ['./user-create.component.css']
 })
-export class UserCreateComponent {
-  user: User = new User();
+export class UserCreateComponent implements OnInit {
+  userForm!: FormGroup;
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  private initForm(): void {
+    this.userForm = this.fb.group({
+      name: ['', [Validators.required]],
+      age: ['', [Validators.required, Validators.min(1), Validators.max(120)]]
+    });
+  }
 
   onSubmit(): void {
-    this.userService.createUser(this.user).subscribe(response => {
-      console.log('User created:', response);
-      this.user = new User();
-    });
+    if (this.userForm.valid) {
+      const user: User = this.userForm.value as User;
+      this.userService.createUser(user).subscribe({
+        next: (response) => {
+          console.log('User created:', response);
+          this.snackBar.open('User created successfully!', 'Close', { duration: 3000 });
+          this.userForm.reset();
+        },
+        error: (error) => {
+          console.error('Error creating user:', error);
+          this.snackBar.open('Error creating user. Please try again.', 'Close', { duration: 3000 });
+        }
+      });
+    }
   }
 }
